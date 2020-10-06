bbf4c683bb863f136ee92cab04693b049f949fba
Max Kaye <m@xk.io>
2020-10-06 16:12:58 +1100

    Add tests, passing

--- test/generator_test.dart
+++ test/generator_test.dart
@@ -1,35 +1,58 @@
 import 'dart:io';
+import 'package:path/path.dart' as p;
 import 'package:build/build.dart';
 import 'package:build_test/build_test.dart';
+import 'package:source_gen/source_gen.dart';
 import 'package:source_gen_test/source_gen_test.dart';
 import 'package:web3_contract_generator/generator.dart';
 import 'package:test/test.dart';
 import 'package:web3dart/credentials.dart';
 
-void main() async {
-  test('web3 generator handles our own content', () async {
-    // var reader = await ('test', 'empty.abi');
+final FILES_TO_TEST = <String>[
+  'empty',
+  'SvEnsIface',
+  'SVIndex',
+  'SVIndexBackend',
+  'SVPayments',
+].map((fn) => p.joinAll(['test', 'contracts', fn]));
 
-    print('running web3 generator test');
-    await testBuilder(
-      Web3ContractGenerator(),
-      sources,
-      reader: await PackageAssetReader.currentIsolate(
-          rootPackage: 'web3_contract_generator'),
-      generateFor: {'**/*.abi'},
-      outputs: outputs,
-    );
-  });
+void main() async {
+  initializeBuildLogTracking();
+
+  /*
+  final readers = await Future.forEach<String>(
+      FILES_TO_TEST,
+      (fn) => initializeLibraryReaderForDirectory(
+          p.join('test', 'contracts'), '${fn}.abi')) as List<LibraryReader>;
+
+  for (var reader in readers) {
+    print('reader.element: ${reader.element}');
+    print(reader);
+    print(reader.allElements);
+    print(reader.findType('abi'));
+    await testBuilderOnFile(
+        reader, Web3ContractGenerator(nonNullableEnabled: false));
+  }
+  */
+
+  final sources = Map<String, String>.fromEntries(
+      FILES_TO_TEST.map((fn) => MapEntry(fn, readFile('${fn}.abi'))));
+
+  final outputs = Map.fromEntries(
+      FILES_TO_TEST.map((fn) => MapEntry(fn, readFile('${fn}.w3.dart'))));
+
+  for (var k in sources.keys) {
+    test('web3 generator handles ${k}', () async {
+      expect(genWholeW3ContractFile(sources[k], p.split('${k}.abi').last),
+          outputs[k]);
+    });
+  }
 }
 
 const _pkgName = 'web3';
 
-final sources = Map.fromEntries([
-  MapEntry('$_pkgName|test/empty.abi', readFile('test/empty.abi'))
-]); // {'$_pkgName|lib/contracts/empty.abi': '[]'};
-
-const outputs = {'$_pkgName|lib/contracts/empty.w3.dart': ''};
-
 String readFile(String path) {
   return File(path).readAsStringSync();
 }
+
+Future<void> testBuilderOnFile(LibraryReader reader, Builder generator) async {}
